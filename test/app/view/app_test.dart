0b7aee2db3b59e3f78ee89a59b9b75f0058da724
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-01-21 20:56:27 +0530

    refactor: remove unused file & change test case

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sparkify/app/app.dart';
-import 'package:sparkify/counter/counter.dart';
+import 'package:sparkify/features/authentication/view/pages/login_page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(LoginPage), findsOneWidget);
     });
   });
 }
